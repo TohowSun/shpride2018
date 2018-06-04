011275d96d5a8f96c20117a47271b3500f64da04
Rae Sun <sunr@vmware.com>
2018-06-04 18:21:58 +0800

    add shpff page

---
+++ page/component/pages/shpff/shpff.js
@@ -0,0 +1,95 @@
+// page/component/pages/shpff/shpff.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        id: '20180609',
+        name: '2018-06-09 周六 SAT',
+        open: false,
+        pages: [
+          {
+            id: 'pride-run',
+            name: '骄傲跑',
+            nameEn: 'Pride Run',
+            time: '9:00-11:00',
+            address: '终点站：上海新天地安达仕酒店',
+          }
+        ]
+      }
+    ]
+  },
+
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
